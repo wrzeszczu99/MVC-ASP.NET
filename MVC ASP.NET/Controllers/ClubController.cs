9e1cafb07c430f312c3c192189a9c0d6ea8a8d1b
Wrzeszczu <wrzeszcz.aleksander@gmail.com>
2024-01-06 14:12:21 +0100

    Controllers

---
+++ MVC ASP.NET/Controllers/ClubController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_ASP.NET.Data;
+using MVC_ASP.NET.Models;
+
+namespace MVC_ASP.NET.Controllers
+{
+    public class ClubController : Controller
+    {
+		private readonly ApplicationDBContext _context;
+
+		public ClubController(ApplicationDBContext context)
+        {
+			_context = context;
+		}
+        public IActionResult Index()
+        {
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Club club = _context.Clubs.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
+            return View(club);
+        }
+    }
+}
---
+++ MVC ASP.NET/Controllers/RaceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_ASP.NET.Data;
+using MVC_ASP.NET.Models;
+
+namespace MVC_ASP.NET.Controllers
+{
+    public class RaceController : Controller
+    {
+		private readonly ApplicationDBContext _context;
+
+		public RaceController(ApplicationDBContext context) {
+			_context = context;
+		}
+        public IActionResult Index()
+        {
+            var races = _context.Races.ToList();
+            return View(races);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Race race = _context.Races.Include(a => a.Address).FirstOrDefault(r => r.Id == id);
+            return View(race);
+        }
+    }
+}
