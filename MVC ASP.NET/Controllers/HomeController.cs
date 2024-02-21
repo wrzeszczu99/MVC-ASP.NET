32e03e4d9b7c89d5c76e32c57e8bf06eed8fa7ec
Wrzeszczu <wrzeszcz.aleksander@gmail.com>
2024-02-21 10:38:09 +0100

    IP geolocation with ipinfo.io

--- MVC ASP.NET/Controllers/HomeController.cs
+++ MVC ASP.NET/Controllers/HomeController.cs
@@ -1,21 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_ASP.NET.Helpers;
+using MVC_ASP.NET.Interfaces;
 using MVC_ASP.NET.Models;
+using MVC_ASP.NET.ViewModel;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 namespace MVC_ASP.NET.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IClubRepository clubRepository)
         {
             _logger = logger;
+            _clubRepository = clubRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IPInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url = "https://ipinfo.io?token="; //Use your token, I won't give you mine https://ipinfo.io
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo regionInfo = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = regionInfo.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+                if(homeViewModel.City != null)
+                {
+                    homeViewModel.Clubs = await _clubRepository.GetClubsByCity(homeViewModel.City);
+                }
+                else
+                {
+                    homeViewModel.Clubs = null;
+                }
+                return View(homeViewModel);
+
+            }
+            catch (Exception e)
+            {
+                homeViewModel.Clubs = null;
+
+                throw;
+            }
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ MVC ASP.NET/ViewModel/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MVC_ASP.NET.Models;
+
+namespace MVC_ASP.NET.ViewModel
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Club> Clubs { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
